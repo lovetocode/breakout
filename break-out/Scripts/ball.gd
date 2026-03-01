ce0e914efcc40710141535f94a6e7b53b6c6b280
lovetocode <dshaggy28@yahoo.com>
2026-02-28 17:05:08 -0800

    set up the ball and paddle

---
+++ break-out/Scripts/ball.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+var dir = Vector2.DOWN
+var is_active = true
+
+func _ready() -> void:
+	velocity = Vector2(SPEED * -1, SPEED)
+	
+func _physics_process(delta: float) -> void:
+	if is_active:
+		
+		var collision = move_and_collide(velocity * delta)
+		
+		if collision:
+			velocity = velocity.bounce(collision.get_normal())
+			
+			
+		if velocity.y > 0 and velocity.y < 100:
+			velocity.y = -200
+		
+		if velocity.x == 0:
+			velocity.x == -200
